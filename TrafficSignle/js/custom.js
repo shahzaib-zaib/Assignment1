e42ed04e3ae4ebf5b099608babba2a802f12110f
Shahzaib <shahzaibux@gmail.com>
2020-03-30 20:00:31 +0500

    Added JavaScript

---
+++ TrafficSignle/js/custom.js
@@ -0,0 +1,28 @@
+function stop(){
+    document.getElementById('red').className = 'redone';
+    document.getElementById('yellow').className = 'yellow';
+    document.getElementById('green').className = 'green';
+}
+function readytogo(){
+   document.getElementById('yellow').className = 'yellowone';
+   document.getElementById('red').className = 'red';
+   document.getElementById('green').className = 'green';
+}
+function go(){
+   document.getElementById('green').className = 'greenone';
+   document.getElementById('red').className = 'red';
+   document.getElementById('yellow').className = 'yellow';
+}
+function stopp(){
+   var one =  document.getElementById("red").className = "red";
+   var two =  document.getElementById("yellow").className = "yellow";
+   var three =  document.getElementById("green").className = "green"; 
+}
+function start(){
+  var one =  document.getElementById("red").className = "one";
+  var two =  document.getElementById("yellow").className = "two";
+  var three =  document.getElementById("green").className = "three";   
+}
+function reset(){
+   
+}
